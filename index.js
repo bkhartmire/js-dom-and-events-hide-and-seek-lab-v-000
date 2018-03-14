aa6ceab08b0da11ea3fc82ba14266ffe4caab93c
bkhartmire <bkhartmire@gmail.com>
2018-03-14 21:35:04 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll('ul.ranked-list li')
+  for (let i=0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
